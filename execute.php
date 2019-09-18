3159d640c438462bc2ac80f84db3b6e666205e4d
fbeniamino <fbeniamino@imolainformatica.it>
2019-09-18 22:21:39 +0200

    probosciabot

--- execute.php
+++ execute.php
@@ -40,7 +40,7 @@ if($text == "/slotmachine"){
 	$res =  "din din din din din din din din din din";
 	header("Content-Type: application/json");
 	$parameters = array('chat_id' => $chatId, "text" => $res);
-	$parameters["photo"] = "https://media2.giphy.com/media/26BRFrAgGF6wtebio/giphy.gif"
+	$parameters["photo"] = "https://media2.giphy.com/media/26BRFrAgGF6wtebio/giphy.gif";
 	$parameters["method"] = "sendMessage";
 	echo json_encode($parameters);
 }
